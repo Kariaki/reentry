b22b45877429ea298368d55712a699fbb3391644
Hanniel <66899695+HANNIEL21@users.noreply.github.com>
2024-10-08 12:15:16 +0100

    Hanniel (#5)
    * Feat: added color class and static colors
    
    * created my branch

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const black = Colors.black;
+  static const white = Colors.white;
+  static const base = Color(0xFF3AE6BD);
+  static const gray1 = Color(0xFF1C1C1C);
+  static const gray2 = Color(0xFF828282);
+}
