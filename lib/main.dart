60a241cfa622485cfedaa82701334f8f9a92bf04
Hanniel <66899695+HANNIEL21@users.noreply.github.com>
2024-10-10 16:13:10 +0100

    Fonts (#9)
    * Feat: added color class and static colors
    
    * created my branch
    
    * FEAT: added icons and flutter_svg package
    
    * Feat: added custom icon and created AppTestStyle class
    
    * started
    
    * snapshot
    
    ---------
    
    Co-authored-by: ebilate <kariaki.ebilate@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -19,16 +19,42 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: Scaffold(
-        body: Center(
-          child: Container(
-            height: 75,
-            width: 75,
-            color: AppColors.gray2,
+        backgroundColor: AppColors.black,
+        body: Center(  // Wrap the entire body in Center to center everything
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,  // Vertically center content
+            crossAxisAlignment: CrossAxisAlignment.center,  // Horizontally center content
+            children: [
+              Container(
+                height: 75,
+                width: 75,
+                color: AppColors.gray2,
+              ),
+              SizedBox(height: 20),  // Spacing before Wrap
+              Wrap(
+                spacing: 20.0,  // Horizontal spacing between icons
+                runSpacing: 20.0,  // Vertical spacing between rows
+                alignment: WrapAlignment.center,  // Center icons horizontally
+                children: [
+                  // SvgPicture.asset(AppIcons.apple, height: 50, width: 50),
+                  // SvgPicture.asset(AppIcons.activity, height: 50, width: 50),
+                  // SvgPicture.asset(AppIcons.addOutline, height: 50, width: 50),
+                  // SvgPicture.asset(AppIcons.bin, height: 50, width: 50),
+                  // SvgPicture.asset(AppIcons.chatBubble, height: 50, width: 50),
+                  // SvgPicture.asset(AppIcons.chevronLeft, height: 50, width: 50),
+                  // SvgPicture.asset(AppIcons.eyeHide, height: 50, width: 50),
+                  // SvgPicture.asset(AppIcons.greenCheck, height: 50, width: 50),
+                  // SvgPicture.asset(AppIcons.google, height: 50, width: 50),
+                  // SvgPicture.asset(AppIcons.home, height: 50, width: 50),
+                  // SvgPicture.asset(AppIcons.mailOutline, height: 50, width: 50),
+                  // SvgPicture.asset(AppIcons.pen, height: 50, width: 50),
+                  // Add more icons as needed
+                ],
+              ),
+            ],
           ),
         ),
-      ),
+      )
     );
   }
 }
-
-
