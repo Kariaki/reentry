180b25b1e45adbabb19249f41172ed646d9dde16
ebilate <kariaki.ebilate@gmail.com>
2024-10-20 07:10:07 +0100

    added user id

--- lib/ui/modules/mentor/bloc/mentor_bloc.dart
+++ lib/ui/modules/mentor/bloc/mentor_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:reentry/data/repository/mentor/mentor_repository.dart';
+import 'package:reentry/data/shared/share_preference.dart';
 import 'package:reentry/ui/modules/mentor/bloc/mentor_event.dart';
 import 'package:reentry/ui/modules/mentor/bloc/mentor_state.dart';
 
@@ -14,7 +15,13 @@ class MentorBloc extends Bloc<MentorEvent, MentorState> {
       RequestMentorEvent event, Emitter<MentorState> emit) async {
     emit(MentorStateLoading());
     try {
-      final result = await _repo.requestMentor(event.data);
+      final user = await PersistentStorage.getCurrentUser();
+      if (user == null) {
+        emit(MentorStateError('User not found'));
+        return;
+      }
+      final result =
+          await _repo.requestMentor(event.data.copyWith(userId: user.userId));
       emit(MentorStateSuccess(result));
     } catch (e) {
       emit(MentorStateError(e.toString()));
