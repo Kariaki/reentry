9928d11975a734d6648eece3f17b715965b17e3d
codewithsam01 <ajakaye.d@blutechsolutions.com>
2024-11-10 00:46:06 +0100

    web installation is ready!

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:reentry/core/theme/colors.dart';
@@ -18,6 +18,8 @@ import 'package:reentry/ui/modules/messaging/bloc/conversation_cubit.dart';
 import 'package:reentry/ui/modules/messaging/bloc/message_cubit.dart';
 import 'package:reentry/ui/modules/profile/bloc/profile_cubit.dart';
 import 'package:reentry/ui/modules/splash/splash_screen.dart';
+import 'package:reentry/ui/modules/splash/web_splash_screen.dart';
+
 late final FirebaseApp app;
 late final FirebaseAuth auth;
 
@@ -28,16 +30,29 @@ void main() async {
 
 // We store the app and auth to make testing with a named instance easier.
   setupDi();
+
+  final String appId;
+  if (kIsWeb) {
+    appId = "1:277362543199:android:cd75ae50fc9db899a1e9ea";
+  } else if (defaultTargetPlatform == TargetPlatform.android) {
+    appId = "1:277362543199:android:cd75ae50fc9db899a1e9ea";
+  } else if (defaultTargetPlatform == TargetPlatform.iOS) {
+    appId = "1:277362543199:android:cd75ae50fc9db899a1e9ea";
+  } else {
+    throw UnsupportedError("This platform is not supported");
+  }
+
   app = await Firebase.initializeApp(
-    options:  FirebaseOptions(
+    options: FirebaseOptions(
         apiKey: "AIzaSyDaLHkABOMmrDWZ4qhydqqoQX08XKXP_Zo",
         authDomain: "trs-app-13c75.firebaseapp.com",
         projectId: "trs-app-13c75",
         storageBucket: "trs-app-13c75.appspot.com",
         messagingSenderId: "277362543199",
-        appId: Platform.isAndroid
-            ? "1:277362543199:android:cd75ae50fc9db899a1e9ea"
-            : "1:277362543199:ios:9375181851d87c27a1e9ea",
+        // appId: Platform.isAndroid
+        //     ? "1:277362543199:android:cd75ae50fc9db899a1e9ea"
+        //     : "1:277362543199:ios:9375181851d87c27a1e9ea",
+        appId: appId,
         measurementId: "G-DFNJ45R5R9"),
   );
   runApp(const MyApp());
@@ -54,7 +69,10 @@ class MyApp extends StatelessWidget {
           BlocProvider(create: (context) => AuthBloc()),
           BlocProvider(create: (context) => AccountCubit()),
           BlocProvider(create: (context) => ProfileCubit()),
-          BlocProvider(create: (context) => GoalCubit()..fetchGoals()..fetchHistory()),
+          BlocProvider(
+              create: (context) => GoalCubit()
+                ..fetchGoals()
+                ..fetchHistory()),
           BlocProvider(create: (context) => GoalsBloc()),
           BlocProvider(create: (context) => MessageCubit()),
           BlocProvider(create: (context) => ConversationUsersCubit()),
@@ -71,34 +89,34 @@ class MyApp extends StatelessWidget {
           BlocProvider(create: (context) => RecommendedClientCubit()),
         ],
         child: MaterialApp(
-            title: 'Flutter Demo',
-            debugShowCheckedModeBanner: false,
-            themeMode: ThemeMode.dark,
-            darkTheme: ThemeData(
-                colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
-                useMaterial3: true,
-                appBarTheme:
-                    const AppBarTheme(backgroundColor: AppColors.black),
-                primaryColor: AppColors.primary,
-                bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-                    backgroundColor: AppColors.black),
-                textTheme: const TextTheme(
-                  bodyMedium: TextStyle(color: AppColors.white, fontSize: 14),
-                  displaySmall: TextStyle(color: AppColors.white, fontSize: 12),
-                  bodyLarge: TextStyle(color: AppColors.white, fontSize: 16),
-                  bodySmall: TextStyle(color: AppColors.white, fontSize: 12),
-                  titleLarge: TextStyle(
-                      color: AppColors.primary,
-                      fontSize: 40,
-                      fontFamily: 'InterBold',
-                      fontWeight: FontWeight.bold),
-                  titleSmall: TextStyle(
-                      color: AppColors.white,
-                      fontSize: 18,
-                      fontFamily: 'InterBold'),
-                  titleMedium: TextStyle(color: AppColors.white, fontSize: 20),
-                ),
-                fontFamily: 'Inter'),
-            home: const SplashScreen()));
+          title: 'Flutter Demo',
+          debugShowCheckedModeBanner: false,
+          themeMode: ThemeMode.dark,
+          darkTheme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
+              useMaterial3: true,
+              appBarTheme: const AppBarTheme(backgroundColor: AppColors.black),
+              primaryColor: AppColors.primary,
+              bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+                  backgroundColor: AppColors.black),
+              textTheme: const TextTheme(
+                bodyMedium: TextStyle(color: AppColors.white, fontSize: 14),
+                displaySmall: TextStyle(color: AppColors.white, fontSize: 12),
+                bodyLarge: TextStyle(color: AppColors.white, fontSize: 16),
+                bodySmall: TextStyle(color: AppColors.white, fontSize: 12),
+                titleLarge: TextStyle(
+                    color: AppColors.primary,
+                    fontSize: 40,
+                    fontFamily: 'InterBold',
+                    fontWeight: FontWeight.bold),
+                titleSmall: TextStyle(
+                    color: AppColors.white,
+                    fontSize: 18,
+                    fontFamily: 'InterBold'),
+                titleMedium: TextStyle(color: AppColors.white, fontSize: 20),
+              ),
+              fontFamily: 'Inter'),
+          home: kIsWeb ? const WebSplashScreen() : const SplashScreen(),
+        ));
   }
 }
