5491a0079f1679dd4a4477d2249c154d7dedc5e2
ebilate <kariaki.ebilate@gmail.com>
2024-11-19 15:29:36 +0100

    snapshot

--- lib/ui/modules/blog/bloc/blog_state.dart
+++ lib/ui/modules/blog/bloc/blog_state.dart
@@ -18,24 +18,30 @@ class CreateBlogContentSuccess extends BlogState {}
 class BlogCubitState {
   final CubitState state;
   final List<BlogDto> data;
+  final BlogDto? currentBlog;
 
-  BlogCubitState({this.data = const [], required this.state});
+  BlogCubitState({this.data = const [], required this.state, this.currentBlog});
 
   static BlogCubitState init() => BlogCubitState(
         state: CubitState(),
       );
 
-  BlogCubitState loading() => BlogCubitState(
-        state: CubitStateLoading(),
-      );
+  BlogCubitState loading() =>
+      BlogCubitState(state: CubitStateLoading(), currentBlog: currentBlog);
 
-  BlogCubitState success({List<BlogDto>? data}) =>
-      BlogCubitState(data: data ?? this.data, state: CubitStateSuccess());
+  BlogCubitState success({List<BlogDto>? data, BlogDto? currentBlog}) =>
+      BlogCubitState(
+          data: data ?? this.data,
+          state: CubitStateSuccess(),
+          currentBlog: currentBlog ?? this.currentBlog);
 
-  BlogCubitState error(String error) =>
-      BlogCubitState(state: CubitStateError(error));
+  BlogCubitState error(String error) => BlogCubitState(
+      state: CubitStateError(error), data: data, currentBlog: currentBlog);
+
   bool get isLoading => state is CubitStateLoading;
+
   bool get isSuccess => state is CubitStateSuccess;
+
   bool get isError => state is CubitStateError;
 
   String get errorMessage =>
