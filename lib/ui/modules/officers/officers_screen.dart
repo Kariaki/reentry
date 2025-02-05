c1519f5e30047b5969a1b59cbbe15611b9ad20ac
D-realsamdev <ajakayedamilola7@gmail.com>
2025-02-05 01:28:45 +0100

    Update officers_screen.dart

--- lib/ui/modules/officers/officers_screen.dart
+++ lib/ui/modules/officers/officers_screen.dart
@@ -88,6 +88,7 @@ class _CareTeamScreenState extends State<CareTeamScreen> {
   @override
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
+    bool isMobile = screenWidth < 600;
     int crossAxisCount = 5;
     if (screenWidth < 1200) {
       crossAxisCount = 4;
@@ -103,149 +104,127 @@ class _CareTeamScreenState extends State<CareTeamScreen> {
       create: (context) =>
           AdminUserCubitNew()..fetchUserCareTeam(widget.accountType),
       child: BlocBuilder<AdminUserCubitNew, MentorDataState>(
-        builder: (context,_state) {
-          final state = _state.state;
-          return BaseScaffold(
-            isLoading: state is CubitStateLoading,
-            appBar: PreferredSize(
-              preferredSize: const Size.fromHeight(120),
-              child: AppBar(
-                backgroundColor: AppColors.greyDark,
-                flexibleSpace: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Search",
-                        style: context.textTheme.bodyLarge?.copyWith(
-                          color: AppColors.greyWhite,
-                          fontWeight: FontWeight.w700,
-                        ),
+          builder: (context, _state) {
+        final state = _state.state;
+        return BaseScaffold(
+          isLoading: state is CubitStateLoading,
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(120),
+            child: AppBar(
+              backgroundColor: AppColors.greyDark,
+              flexibleSpace: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Search",
+                      style: context.textTheme.bodyLarge?.copyWith(
+                        color: AppColors.greyWhite,
+                        fontWeight: FontWeight.w700,
                       ),
-                      const SizedBox(
-                        height: 10,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    InputField(
+                      controller: _searchController,
+                      hint: 'Enter name or email to search',
+                      radius: 10.0,
+                      preffixIcon: const Icon(
+                        CupertinoIcons.search,
+                        color: AppColors.white,
                       ),
-                      InputField(
-                        controller: _searchController,
-                        hint: 'Enter name or email to search',
-                        radius: 10.0,
-                        preffixIcon: const Icon(
-                          CupertinoIcons.search,
-                          color: AppColors.white,
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
-            child: Expanded(child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Builder(
-                  builder: (context,) {
+          ),
+          child: Expanded(
+              child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Builder(
+                builder: (
+                  context,
+                ) {
+                  if (state is CubitStateLoading) {
+                    return SizedBox();
+                  }
+                  if (state is CubitStateError) {
+                    return Center(
+                      child: Text(
+                        "Error: ${state.message}",
+                        style: context.textTheme.bodyLarge?.copyWith(
+                          color: AppColors.red,
+                        ),
+                      ),
+                    );
+                  }
 
-                    if(state is CubitStateLoading){
-                      return SizedBox();
-                    }
-                    if (state is CubitStateError) {
-                      return Center(
-                        child: Text(
-                          "Error: ${state.message}",
-                          style: context.textTheme.bodyLarge?.copyWith(
-                            color: AppColors.red,
+                  final data = _state.data;
+                  if (data.isEmpty) {
+                    return Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.people_outline,
+                            size: 100,
+                            color: AppColors.greyWhite,
                           ),
-                        ),
-                      );
-                    }
-
-                    final data = _state.data;
-                    if (data.isEmpty) {
-                      return Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Icon(
-                              Icons.people_outline,
-                              size: 100,
+                          const SizedBox(height: 20),
+                          Text(
+                            "No mentors available",
+                            style: context.textTheme.bodyLarge?.copyWith(
                               color: AppColors.greyWhite,
+                              fontWeight: FontWeight.w600,
                             ),
-                            const SizedBox(height: 20),
-                            Text(
-                              "No mentors available",
-                              style: context.textTheme.bodyLarge?.copyWith(
-                                color: AppColors.greyWhite,
-                                fontWeight: FontWeight.w600,
-                              ),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            "Try searching for a term or check back later.",
+                            textAlign: TextAlign.center,
+                            style: context.textTheme.bodySmall?.copyWith(
+                              color: AppColors.gray2,
                             ),
-                            const SizedBox(height: 10),
-                            Text(
-                              "Try searching for a term or check back later.",
-                              textAlign: TextAlign.center,
-                              style: context.textTheme.bodySmall?.copyWith(
-                                color: AppColors.gray2,
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    }
-                    final mentorList = filterMentors(data);
-                    final totalPages = (mentorList.length / itemsPerPage).ceil();
-                    final paginatedItems = getPaginatedItems(mentorList);
-                    final columns = [
-                      const DataColumn(label: TableHeader("Name")),
-                      const DataColumn(label: TableHeader("Email")),
-                      const DataColumn(label: TableHeader("DOB")),
-                      const DataColumn(label: TableHeader("Date Joined")),
-                    ];
-                    List<DataRow> _buildRows(context) {
-                      return paginatedItems.map((item) {
-                        return DataRow(
-                          onSelectChanged: (isSelected) {
-                            _navigate(item);
-                          },
-                          cells: [
-                            DataCell(Row(
-                              mainAxisSize: MainAxisSize.min,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                SizedBox(
-                                  width: 20,
-                                  height: 20,
-                                  child: CircleAvatar(
-                                    backgroundImage: NetworkImage(
-                                        item.avatar ?? AppConstants.avatar),
-                                  ),
-                                ),
-                                10.width,
-                                Text(item.name)
-                              ],
-                            )),
-                            DataCell(Text(item.email)),
-                            DataCell(Text(DateTime.tryParse(item.dob ?? '')?.formatDate()??'')),
-                            DataCell(Text(item.createdAt ?? '')),
-                          ],
-                        );
-                      }).toList();
-                    }
+                          ),
+                        ],
+                      ),
+                    );
+                  }
+                  final mentorList = filterMentors(data);
+                  final totalPages = (mentorList.length / itemsPerPage).ceil();
+                  final paginatedItems = getPaginatedItems(mentorList);
 
-                    final rows = _buildRows(context);
-
+                  if (isMobile) {
                     return Column(
                       children: [
-                        Container(
-                          color: Colors.black,
-                          child: ReusableTable(
-                            columns: columns,
-                            rows: rows,
-                            headingRowColor: AppColors.white,
-                            dataRowColor: AppColors.greyDark,
-                            columnSpacing: 20.0,
-                            dataRowHeight: 56.0,
+                        GridView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          gridDelegate:
+                              const SliverGridDelegateWithMaxCrossAxisExtent(
+                            maxCrossAxisExtent: 300,
+                            crossAxisSpacing: 10,
+                            mainAxisSpacing: 10,
+                            childAspectRatio: 3 / 4,
                           ),
+                          itemCount: paginatedItems.length,
+                          itemBuilder: (context, index) {
+                            final item = paginatedItems[index];
+                            return GestureDetector(
+                              onTap: () => _navigate(item),
+                              child: ProfileCard(
+                                email: item.email ?? '',
+                                name: item.name,
+                                imageUrl: item.avatar ?? '',
+                                showActions: false,
+                              ),
+                            );
+                          },
                         ),
                         const SizedBox(height: 20),
                         Pagination(
@@ -255,24 +234,86 @@ class _CareTeamScreenState extends State<CareTeamScreen> {
                         ),
                       ],
                     );
-                  },
-                ),
+                  }
+
+                  final columns = [
+                    const DataColumn(label: TableHeader("Name")),
+                    const DataColumn(label: TableHeader("Email")),
+                    const DataColumn(label: TableHeader("DOB")),
+                    const DataColumn(label: TableHeader("Date Joined")),
+                  ];
+                  List<DataRow> _buildRows(context) {
+                    return paginatedItems.map((item) {
+                      return DataRow(
+                        onSelectChanged: (isSelected) {
+                          _navigate(item);
+                        },
+                        cells: [
+                          DataCell(Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                width: 20,
+                                height: 20,
+                                child: CircleAvatar(
+                                  backgroundImage: NetworkImage(
+                                      item.avatar ?? AppConstants.avatar),
+                                ),
+                              ),
+                              10.width,
+                              Text(item.name)
+                            ],
+                          )),
+                          DataCell(Text(item.email)),
+                          DataCell(Text(
+                              DateTime.tryParse(item.dob ?? '')?.formatDate() ??
+                                  '')),
+                          DataCell(Text(item.createdAt ?? '')),
+                        ],
+                      );
+                    }).toList();
+                  }
+
+                  final rows = _buildRows(context);
+
+                  return Column(
+                    children: [
+                      Container(
+                        color: Colors.black,
+                        child: ReusableTable(
+                          columns: columns,
+                          rows: rows,
+                          headingRowColor: AppColors.white,
+                          dataRowColor: AppColors.greyDark,
+                          columnSpacing: 20.0,
+                          dataRowHeight: 56.0,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Pagination(
+                        totalPages: totalPages,
+                        currentPage: currentPage,
+                        onPageSelected: setPage,
+                      ),
+                    ],
+                  );
+                },
               ),
-            )),
-          );
-        }
-      ),
+            ),
+          )),
+        );
+      }),
     );
   }
 
   _navigate(UserDto profile) async {
     context.read<AdminUserCubitNew>().selectCurrentUser(profile);
     context.goNamed(
-     widget.accountType==AccountType.mentor? AppRoutes.mentorProfile.name:AppRoutes.officersProfile.name,
+        widget.accountType == AccountType.mentor
+            ? AppRoutes.mentorProfile.name
+            : AppRoutes.officersProfile.name,
         extra: profile.userId,
-        queryParameters: {
-          'id':profile.userId
-        }
-    );
+        queryParameters: {'id': profile.userId});
   }
 }
