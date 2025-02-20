84210c59aec7bf49d25ed68cf6bfdce51ad368f1
ebilate <kariaki.ebilate@gmail.com>
2025-02-20 16:34:24 +0100

    done

--- lib/ui/modules/authentication/web/web_user_info_screen.dart
+++ lib/ui/modules/authentication/web/web_user_info_screen.dart
@@ -28,19 +28,23 @@ class WebOnboardingBasicUserInfo extends HookWidget {
   Widget build(BuildContext context) {
     final key = useMemoized(() => GlobalKey<FormState>());
 
-    final data = context.read<OnboardingCubit>().state??OnboardingEntity(email: 'email');
+    final data = context.read<OnboardingCubit>().state ??
+        OnboardingEntity(email: 'email');
     final date = useState<DateTime?>(DateTime(2000));
     final selectedAccountType = useState<int?>(data.accountType?.index);
+    final accountType = useState(AccountType.citizen);
     final nameController = useTextEditingController(text: data.name);
     final addressController = useTextEditingController(text: data.address);
     final phoneController = useTextEditingController(text: data.phoneNumber);
+    final teamLeaderController =
+        useTextEditingController(text: data.phoneNumber);
+
+    final selectedServices = useState<Set<String>>({});
     return BlocConsumer<AuthBloc, AuthState>(
       listener: (context, state) {
         if (state is RegistrationSuccessFull) {
-
           context.showSnackbarSuccess('Account created successfully');
-          context.goNamed(AppRoutes.success.name);
-          if (state.data.accountType==AccountType.citizen) {
+          if (state.data.accountType == AccountType.citizen || state.data.accountType==AccountType.reentry_orgs) {
             context.goNamed(AppRoutes.dashboard.name);
           }
         }
@@ -68,59 +72,19 @@ class WebOnboardingBasicUserInfo extends HookWidget {
                             crossAxisAlignment: CrossAxisAlignment.start,
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              Text("Let's get you set!",style: Theme.of(context).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold),),
-                              5.height,
-                              Text("Enter your details to set your profile",style: Theme.of(context).textTheme.bodyMedium,),
-                              50.height,
-                              InputField(
-                                hint: 'First name Last name',
-                                validator: InputValidators.stringValidation,
-                                enable: data.name == null,
-                                label: 'Full name',
-                                controller: nameController,
-                              ),
-                              15.height,
-                              InputField(
-                                hint: 'Address',
-                                validator: (v) => null,
-                                label: 'Street, City, State',
-                                controller: addressController,
-                              ),
-                              15.height,
-                              InputField(
-                                hint: '(000) 000-0000',
-                                controller: phoneController,
-                                enable: true,
-                                phone: true,
-                                label: "Phone number",
-                              ),
-                              15.height,
                               Text(
-                                'Date of birth',
-                                style: AppTextStyle.heading.copyWith(
-                                    color: AppColors.white, fontSize: 14),
+                                "Let's get you set!",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleMedium
+                                    ?.copyWith(fontWeight: FontWeight.bold),
                               ),
-                              8.height,
-                              DateTimePicker(
-                                hint: 'Date of birth',
-                                height: 12,
-                                title: date.value?.formatDate(),
-                                radius: 50,
-                                onTap: () async {
-                                  final result = await showDatePicker(
-                                      initialDate: DateTime(2010),
-                                      firstDate: DateTime(2004),
-                                      lastDate: DateTime(2024),
-                                      onDatePickerModeChange: (value) {},
-                                      context: context);
-                                  if (result == null) {
-                                    return;
-                                  }
-                                  date.value = result;
-                                },
-                                // title: date.value?.formatDate(),
+                              5.height,
+                              Text(
+                                "Enter your details to set your profile",
+                                style: Theme.of(context).textTheme.bodyMedium,
                               ),
-                              15.height,
+                              50.height,
                               Row(
                                 children: [
                                   Text(
@@ -142,33 +106,185 @@ class WebOnboardingBasicUserInfo extends HookWidget {
                               10.height,
                               PillSelector(
                                 options: AppConstants.accountType,
-                                initialSelectedItemIndex: selectedAccountType.value??0,
+                                initialSelectedItemIndex:
+                                    selectedAccountType.value ?? 0,
                                 onChange: (value) {
                                   selectedAccountType.value = value;
+                                  accountType.value = AccountType.values[value];
                                 },
                                 wrap: true,
                               ),
+                              15.height,
+                              InputField(
+                                hint: accountType.value ==
+                                        AccountType.reentry_orgs
+                                    ? 'Organization name'
+                                    : 'First name Last name',
+                                validator: InputValidators.stringValidation,
+                                enable: data.name == null,
+                                label: accountType.value ==
+                                        AccountType.reentry_orgs
+                                    ? 'Organization name'
+                                    : 'Full name',
+                                controller: nameController,
+                              ),
+                              15.height,
+                              InputField(
+                                label: accountType.value ==
+                                        AccountType.reentry_orgs
+                                    ? 'Organization address'
+                                    : 'Address',
+
+                                validator:accountType.value==AccountType.reentry_orgs? InputValidators.stringValidation:null,
+                                hint: 'Street, City, State',
+                                controller: addressController,
+                              ),
+                              15.height,
+                              InputField(
+                                hint: '(000) 000-0000',
+                                controller: phoneController,
+                                enable: true,
+
+                                validator:accountType.value==AccountType.reentry_orgs? InputValidators.stringValidation:null,
+                                phone: true,
+                                label: accountType.value ==
+                                        AccountType.reentry_orgs
+                                    ? 'Contact phone number'
+                                    : "Phone number",
+                              ),
+                              15.height,
+                              if (accountType.value ==
+                                  AccountType.reentry_orgs) ...[
+                                15.height,
+                                InputField(
+                                  label: 'Team leader ',
+                                  controller: teamLeaderController,
+                                  enable: true,
+                                  validator: InputValidators.stringValidation,
+                                  hint: "First name, Last name",
+                                ),
+                                15.height,
+                                Text(
+                                  'What services do you offer?',
+                                  style: AppTextStyle.heading.copyWith(
+                                      color: AppColors.white, fontSize: 14),
+                                ),
+                                10.height,
+                                Wrap(
+                                  children: List.generate(
+                                      AppConstants.careTeamServices.length,
+                                      (index) {
+                                    final e =
+                                        AppConstants.careTeamServices[index];
+                                    return PillSelectorComponent1(
+                                        selected:
+                                            selectedServices.value.contains(e),
+                                        text: e,
+                                        callback: () {
+                                          if (selectedServices.value
+                                              .contains(e)) {
+                                            selectedServices.value =
+                                                selectedServices.value
+                                                    .where(
+                                                        (value) => value != e)
+                                                    .toSet();
+                                            return;
+                                          }
+                                          // if(selectedServices.value.length==4){
+                                          //   return;
+                                          // }
+                                          selectedServices.value = {
+                                            ...selectedServices.value,
+                                            e
+                                          };
+                                        });
+                                  }).toList(),
+                                ),
+                              ],
+                              if (accountType.value !=
+                                  AccountType.reentry_orgs) ...[
+                                Text(
+                                  'Date of birth',
+                                  style: AppTextStyle.heading.copyWith(
+                                      color: AppColors.white, fontSize: 14),
+                                ),
+                                8.height,
+                                DateTimePicker(
+                                  hint: 'Date of birth',
+                                  height: 12,
+                                  title: date.value?.formatDate(),
+                                  radius: 50,
+                                  onTap: () async {
+                                    final result = await showDatePicker(
+                                        initialDate: DateTime(2010),
+                                        firstDate: DateTime(2004),
+                                        lastDate: DateTime(2024),
+                                        onDatePickerModeChange: (value) {},
+                                        context: context);
+                                    if (result == null) {
+                                      return;
+                                    }
+                                    date.value = result;
+                                  },
+                                  // title: date.value?.formatDate(),
+                                ),
+                              ],
                               50.height,
                               PrimaryButton(
                                 text: 'Continue',
                                 loading: state is AuthLoading,
                                 onPress: () {
                                   if (key.currentState!.validate()) {
-                                    if(selectedAccountType.value==null){
-                                      context.showSnackbarError('Please select profile type');
+                                    if (selectedAccountType.value == null) {
+                                      context.showSnackbarError(
+                                          'Please select profile type');
                                       return;
                                     }
+                                    if (accountType.value ==
+                                        AccountType.reentry_orgs) {
+                                      if (selectedServices.value.isEmpty) {
+                                        context.showSnackbarError(
+                                            'Please select a service');
+                                        return;
+                                      }
+                                      final result = data.copyWith(
+                                          name: nameController.text,
+                                          address: addressController.text,
+                                          accountType: AccountType.values[
+                                              selectedAccountType.value ?? 0],
+                                          supervisorsName:
+                                              teamLeaderController.text,
+                                          organization: nameController.text,
+                                          organizationAddress:
+                                              addressController.text,
+                                          supervisorsEmail: '',
+                                          services:
+                                              selectedServices.value.toList(),
+                                          phoneNumber: phoneController.text);
+
+                                      context
+                                          .read<OnboardingCubit>()
+                                          .setOnboarding(result);
+                                      context
+                                          .read<AuthBloc>()
+                                          .add(RegisterEvent(data: result));
+                                      return;
+                                    }
+
                                     final result = data.copyWith(
                                         name: nameController.text,
                                         address: addressController.text,
-                                        accountType: AccountType.values[selectedAccountType.value??0],
+                                        accountType: AccountType.values[
+                                            selectedAccountType.value ?? 0],
                                         dob: date.value?.toIso8601String(),
                                         phoneNumber: phoneController.text);
                                     context
                                         .read<OnboardingCubit>()
                                         .setOnboarding(result);
                                     if (result.accountType ==
-                                        AccountType.citizen) {
+                                            AccountType.citizen ||
+                                        result.accountType ==
+                                            AccountType.reentry_orgs) {
                                       //create account;
                                       context
                                           .read<AuthBloc>()
